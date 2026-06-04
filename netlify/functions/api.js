1d45cfc91eecb0ee562499042da391fa9d6cadd4
buutran210306-lang <buutran210306@gmail.com>
2026-06-04 13:30:20 +0700

    Update api.ts content to ES modules

--- netlify/functions/api.js
+++
@@ -1,8 +0,0 @@
-const serverless = require('serverless-http');
-
-// Trỏ Netlify tìm đến file app.js trong thư mục dist (sau khi build từ src/app.ts)
-const appModule = require('../../dist/app');
-const app = appModule.default || appModule.app || appModule;
-
-// Khởi tạo handler bọc toàn bộ hệ thống Express của bạn
-exports.handler = serverless(app);
